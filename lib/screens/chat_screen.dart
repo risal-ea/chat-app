f3712532e8d8d3073d6f84fec588b2a98064811e
Risal <risal@Risals-MacBook-Pro.local>
2024-09-23 20:27:51 +0530

    UI updated

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,6 +3,8 @@ import 'package:chat_app/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+final _firestore = FirebaseFirestore.instance;
+
 class ChatScreen extends StatefulWidget {
   static const String id = 'chat_screen';
 
@@ -11,8 +13,8 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final messageTextController = TextEditingController();
   final _auth = FirebaseAuth.instance;
-  final _firestore = FirebaseFirestore.instance;
   late User loggedInUser;
   String messageText = '';
 
@@ -33,13 +35,6 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-  // void getmessages()async{
-  //   final messages = await _firestore.collection('messages').get();
-  //   for(var message in  messages.docs){
-  //     print(message.data());
-  //   }
-  // }
-
   void messageStream() async {
     await for (var snapShot in _firestore.collection('messages').snapshots()) {
       for (var message in snapShot.docs) {
@@ -57,8 +52,6 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-                // _auth.signOut();
-                // Navigator.pop(context);
                 messageStream();
               }),
         ],
@@ -70,50 +63,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            StreamBuilder<QuerySnapshot>(
-              stream: _firestore.collection('messages').snapshots(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting){
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-
-                // Handle error cases
-                if (snapshot.hasError) {
-                  return Center(
-                    child: Text('Error loading messages'),
-                  );
-                }
-
-                if (snapshot.hasData) {
-                  final messages = snapshot.data?.docs;
-                  List<Text> messageWidgets = [];
-
-                  for (var message in messages!) {
-                    final messageText = message['text'];
-                    final messageSender = message['sender'];
-
-                    final messageWidget = Text(
-                      '$messageSender: $messageText',
-                      style: TextStyle(color: Colors.white, fontSize: 20.0),
-                    );
-                    messageWidgets.add(messageWidget);
-                  }
-                  return Expanded(
-                    child: ListView(
-                      children: messageWidgets,
-                    ),
-                  );
-                }
-
-                // Fallback if no data is available
-                return Center(
-                  child: Text('No messages to display'),
-                );
-              },
-            ),
-
+            MessagesStream(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -121,6 +71,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messageTextController,
                       onChanged: (value) {
                         messageText = value;
                       },
@@ -129,6 +80,7 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   TextButton(
                     onPressed: () {
+                      messageTextController.clear();
                       _firestore.collection('messages').add({
                         'text': messageText,
                         'sender': loggedInUser.email,
@@ -146,5 +98,92 @@ class _ChatScreenState extends State<ChatScreen> {
         ),
       ),
     );
+  }
+}
+
+class MessagesStream extends StatelessWidget {
+  const MessagesStream({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('messages').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        // Handle error cases
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('Error loading messages'),
+          );
+        }
+
+        if (snapshot.hasData) {
+          final messages = snapshot.data?.docs;
+          List<MessageBubble> messageBubbles = [];
+
+          for (var message in messages!) {
+            final messageText = message['text'];
+            final messageSender = message['sender'];
+
+            final messagBubble =
+            MessageBubble(sender: messageSender, text: messageText);
+            messageBubbles.add(messagBubble);
+          }
+          return Expanded(
+            child: ListView(
+              children: messageBubbles,
+            ),
+          );
+        }
+
+        // Fallback if no data is available
+        return Center(
+          child: Text('No messages to display'),
+        );
+      },
+    );
+  }
+}
+
+
+class MessageBubble extends StatelessWidget {
+  const MessageBubble({super.key, required this.sender, required this.text});
+
+  final String sender;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Text(
+            sender,
+            style: TextStyle(color: Colors.white54),
+          ),
+          Material(
+            borderRadius: BorderRadius.circular(50),
+            elevation: 5.0,
+            color: Colors.lightBlueAccent,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                text,
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+    ;
   }
 }
